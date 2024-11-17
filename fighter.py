105820ead840f0f054688d7ef74910e9280e4da7
Husain Patanwala <h.y.patanwala@gmail.com>
2024-11-17 10:24:56 -0800

    add fighter class and new background

---
+++ fighter.py
@@ -0,0 +1,6 @@
+class Fighter():
+    def __init__(self, x, y, name, max_hp, actions):
+        self.name = name
+        self.max_hp = max_hp
+        self.hp = max_hp
+        self.actions = actions 
--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 import pygame
 import os
+from settings import *
 
 pygame.init()
-screen = pygame.display.set_mode((1000, 500))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Data Structure Destroyer")
 
 
-background = pygame.image.load(os.path.join('Assets', 'Background', 'Zone-202-big.png')).convert_alpha()
-background = pygame.transform.scale(background, (1000, 500))
-
+background = pygame.image.load(os.path.join('Assets','Background','Cavern.png')).convert_alpha()
+background = pygame.transform.scale(background, (1080, 500))
 def draw_bg(): screen.blit(background, (0,0))
 
 
-gameLoop = True
-while gameLoop:
+running = True
+while running:
 
     draw_bg()
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            gameLoop = False
+            running = False
 
     pygame.display.update()
 
